1186ec22c2481bd0d428b3a1568e0ead943ccd12
DantraylS <80242064+DantraylS@users.noreply.github.com>
2023-12-11 21:41:47 -0600

    Finished Video 1 of Swiftul Thinking bootcamp

---
+++ SwiftForBeginners/SwiftForBeginners/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftForBeginners
+//
+//  Created by Dantrayl Sanders on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftForBeginners/SwiftForBeginners/SwiftForBeginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftForBeginnersApp.swift
+//  SwiftForBeginners
+//
+//  Created by Dantrayl Sanders on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftForBeginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
