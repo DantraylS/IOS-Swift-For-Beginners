ef6eb7ec4c49f6baf515260793be3a62168979e0
DantraylS <80242064+DantraylS@users.noreply.github.com>
2023-12-11 22:05:00 -0600

    Finished video 2 of swiftful thinking bootcamp

---
+++ SwiftForBeginners/SwiftForBeginners/TextBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  TextBootcamp.swift
+//  SwiftForBeginners
+//
+//  Created by Dantrayl Sanders on 12/11/23.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text("Hello, World!".capitalized)
+            //.font(.body)
+            //.fontWeight(.semibold)
+            //.bold()
+            //.underline()
+//            .underline(true, color: Color.red)
+//            .italic()
+//            .strikethrough(true, color: Color.green)
+//            .font(.system(size: 24, weight:
+//                    .semibold, design: .serif))
+//            .baselineOffset(10.0)
+//            .kerning(10)
+            .multilineTextAlignment(.leading)
+            .foregroundColor(.red)
+            .frame(width: 200, height: 100, alignment: .leading)
+            .minimumScaleFactor(0.1)
+    }
+}
+
+struct TextBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootcamp()
+    }
+}
